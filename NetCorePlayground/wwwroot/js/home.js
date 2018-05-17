ab63180e0bac3fdea8391d1afef7b74141f2cf13
Caleb Byrd <cbyrd@strategicmarketing.com>
2018-05-17 15:46:11 -0400

    Onclick doesn't work?

---
+++ NetCorePlayground/wwwroot/js/home.js
@@ -0,0 +1,10 @@
+﻿
+    var app = new Vue({
+        el: '#app',
+        data: {
+            app: 'myID',
+            title: "test",
+            Header1: 'Hello Vue!',
+            visiblePTag: true
+        }
+    });
